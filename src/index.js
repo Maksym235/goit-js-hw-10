67dd3624f0953f94cac43683d51bf098319f4db6
Maksym235 <andriichukwork235@gmail.com>
2023-01-03 18:15:54 +0300

    fix task

--- src/index.js
+++ src/index.js
@@ -11,49 +11,61 @@ input.addEventListener('input', debounce(onInputNames, DEBOUNCE_DELAY));
 
 function onInputNames(e) {
   e.preventDefault();
-  const inputValue = e.target.value;
-  const searchQuery = inputValue.trim();
-  if (searchQuery === '') {
-    list.innerHTML = '';
-    countryInfo.innerHTML = '';
+  const searchQuery = e.target.value.trim();
+  clearInput();
+  if (!searchQuery) {
+    return;
   }
   fetchCountries(searchQuery)
-    .then(createMarkup)
+    .then(renderMarkup)
     .catch(err => Notify.failure('Oops, there is no country with that name'));
 }
 
-function createMarkup(arr) {
+function clearInput() {
+  list.innerHTML = '';
+  countryInfo.innerHTML = '';
+}
+
+function renderMarkup(arr) {
   if (arr.length === 1) {
-    const markup = arr
-      .map(
-        item => `<img src="${item.flags.svg}" alt="${
-          item.name.official
-        }" width="60" height="40">
+    createCountryInfoMarkup(arr);
+  } else if (arr.length > 1 && arr.length <= 10) {
+    createCountryListMarkup(arr);
+  } else if (arr.length > 10) {
+    Notify.info('Too many matches found. Please enter a more specific name.');
+  }
+}
+
+function createCountryInfoMarkup(arr) {
+  const markup = arr
+    .map(
+      item => `<img src="${item.flags.svg}" alt="${
+        item.name.official
+      }" width="60" height="40">
     <h2 class="country-title">${item.name.official}</h2>
     <p class="country-descriptoin">Capital: ${item.capital}</p>
     <p class="country-description">Languages: ${Object.values(
       item.languages
     )}</p>
     <p class="country-description">Population: ${item.population}</p>`
-      )
-      .join('');
+    )
+    .join('');
 
-    countryInfo.innerHTML = markup;
-    list.innerHTML = '';
-  } else if (arr.length > 1 && arr.length <= 10) {
-    const markup = arr
-      .map(
-        item => `<li class="country-list__item">
+  countryInfo.innerHTML = markup;
+  list.innerHTML = '';
+}
+
+function createCountryListMarkup(arr) {
+  const markup = arr
+    .map(
+      item => `<li class="country-list__item">
       <img class="country-list__flags" src="${item.flags.svg}" alt="${item.name.official}" width="25" />
       <h2 class="country-list__name">${item.name.official}</h2>
     </li>
     `
-      )
-      .join('');
+    )
+    .join('');
 
-    list.innerHTML = markup;
-    countryInfo.innerHTML = '';
-  } else if (arr.length > 10) {
-    Notify.info('Too many matches found. Please enter a more specific name.');
-  }
+  list.innerHTML = markup;
+  countryInfo.innerHTML = '';
 }
